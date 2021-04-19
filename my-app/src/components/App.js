e0c8005d3cf12c891bfc9b00a5a69c1f93fce803
unknown <aelise17264@gmail.com>
2021-04-18 19:15:22 -0600

    added header, footer & note routes

---
+++ my-app/src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello there</h1>
+
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer(){
+    const year = new Date().getFullYear()
+    return(
+        <footer>
+            <p>
+                Copyright {year}
+            </p>
+        </footer>
+
+    )
+
+}
